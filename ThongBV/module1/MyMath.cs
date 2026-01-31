fa8cb27b2cc3cb96fe75d4faa99c47deb46d6325
thongdzai <buivanthong10052004@gmail.com>
2026-02-01 00:26:04 +0700

    CPL26_Assignment_03: Create Fibonacci with flag = false

--- ThongBV/module1/MyMath.cs
+++ ThongBV/module1/MyMath.cs
@@ -7,6 +7,18 @@ namespace Assignment3
 
         public int Fibonacci(int n, bool flag)
         {
+            if (flag == false)
+            {
+                if (n <= 1) return n;
+                int a = 0, b = 1, c = 0;
+                for (int i = 2; i <= n; i++)
+                {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                }
+                return c;
+            }
 
             return 0;
         }
